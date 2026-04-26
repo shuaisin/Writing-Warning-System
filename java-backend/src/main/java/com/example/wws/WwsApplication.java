fd4f2fae63cbba5afe4cc955d8edb76ea1e546de
shuaisin <shuaisin666888@163.com>
2026-04-26 21:32:08 +0800

    Update WwsApplication.java

--- java-backend/src/main/java/com/example/wws/WwsApplication.java
+++ java-backend/src/main/java/com/example/wws/WwsApplication.java
@@ -3,10 +3,22 @@ package com.example.wws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class WwsApplication {
     public static void main(String[] args) {
         SpringApplication.run(WwsApplication.class, args);
+    }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
